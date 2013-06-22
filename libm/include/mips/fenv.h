1e49ee337f1fdf3f807c896a6db6b42b286de51d
napodan <david.guenet@gmail.com>
2013-06-22 17:23:01 +0200

    Update libm to 4.2.2_r1.2

--- libm/include/mips/fenv.h
+++ libm/include/mips/fenv.h
@@ -29,12 +29,10 @@
 #ifndef	_FENV_H_
 #define	_FENV_H_
 
-#include <sys/types.h>
+#include <sys/_types.h>
 
-__BEGIN_DECLS
-
-typedef	uint32_t	fenv_t;
-typedef	uint32_t	fexcept_t;
+typedef	__uint32_t	fenv_t;
+typedef	__uint32_t	fexcept_t;
 
 /* Exception flags */
 #define	FE_INVALID	0x0001
@@ -52,6 +50,7 @@ typedef	uint32_t	fexcept_t;
 #define	FE_DOWNWARD	0x0003
 #define	_ROUND_MASK	(FE_TONEAREST | FE_DOWNWARD | \
 			 FE_UPWARD | FE_TOWARDZERO)
+__BEGIN_DECLS
 
 /* Default floating-point environment */
 extern const fenv_t	__fe_dfl_env;
