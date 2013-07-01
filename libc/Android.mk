bc3a67282aeecbbe71606b32ce61835ce67224a9
napodan <david.guenet@gmail.com>
2013-07-01 07:29:12 +0200

    Update inet and stdio to 4.2.2_r1.2
    upstream-dlmalloc

--- libc/Android.mk
+++ libc/Android.mk
@@ -140,6 +140,7 @@ libc_common_src_files := \
 	stdio/vsscanf.c \
 	stdio/wbuf.c \
 	stdio/wsetup.c \
+	stdio/__fgets_chk.c \
 	stdio/__snprintf_chk.c \
 	stdio/__sprintf_chk.c \
 	stdio/__vsnprintf_chk.c \
@@ -407,7 +408,7 @@ libc_arch_static_src_files := \
 
 libc_arch_dynamic_src_files := \
 	arch-arm/bionic/exidx_dynamic.c
-else # !arm
+endif # arm
 
 ifeq ($(TARGET_ARCH),x86)
 libc_common_src_files += \
@@ -445,10 +446,53 @@ libc_arch_static_src_files := \
 	arch-x86/bionic/dl_iterate_phdr_static.c
 
 libc_arch_dynamic_src_files :=
-else # !x86
+endif # x86
 
-endif # !x86
-endif # !arm
+ifeq ($(TARGET_ARCH),mips)
+libc_common_src_files += \
+	arch-mips/bionic/__get_sp.S \
+	arch-mips/bionic/__get_tls.c \
+	arch-mips/bionic/__set_tls.c \
+	arch-mips/bionic/_exit_with_stack_teardown.S \
+	arch-mips/bionic/_setjmp.S \
+	arch-mips/bionic/futex_mips.S \
+	arch-mips/bionic/bzero.S \
+	arch-mips/bionic/cacheflush.c \
+	arch-mips/bionic/clone.S \
+	arch-mips/bionic/ffs.S \
+	arch-mips/bionic/memcmp16.S \
+	arch-mips/bionic/memmove.c \
+	arch-mips/bionic/pipe.S \
+	arch-mips/bionic/setjmp.S \
+	arch-mips/bionic/sigsetjmp.S \
+	arch-mips/bionic/vfork.S
+
+libc_common_src_files += \
+	arch-mips/string/memset.S \
+	arch-mips/string/memcpy.S \
+	arch-mips/string/mips_strlen.c
+
+libc_common_src_files += \
+	string/bcopy.c \
+	string/memcmp.c \
+	string/strcmp.c \
+	string/strcpy.c \
+	string/strncmp.c
+
+libc_common_src_files += \
+	bionic/pthread-atfork.c \
+	bionic/pthread-rwlocks.c \
+	bionic/pthread-timers.c \
+	bionic/ptrace.c
+
+libc_static_common_src_files += \
+	bionic/pthread.c
+
+libc_arch_static_src_files := \
+	bionic/dl_iterate_phdr_static.c
+
+libc_arch_dynamic_src_files :=
+endif # mips
 
 # Define some common cflags
 # ========================================================
@@ -510,17 +554,26 @@ ifeq ($(TARGET_ARCH),arm)
   ifeq ($(ARCH_ARM_USE_NON_NEON_MEMCPY),true)
     libc_common_cflags += -DARCH_ARM_USE_NON_NEON_MEMCPY
   endif
-else # !arm
-  ifeq ($(TARGET_ARCH),x86)
-    libc_crt_target_cflags :=
-    ifeq ($(ARCH_X86_HAVE_SSE2),true)
-        libc_crt_target_cflags += -DUSE_SSE2=1
-    endif
-    ifeq ($(ARCH_X86_HAVE_SSSE3),true)
-        libc_crt_target_cflags += -DUSE_SSSE3=1
-    endif
-  endif # x86
 endif # !arm
+
+ifeq ($(TARGET_ARCH),x86)
+  libc_common_cflags += -DSOFTFLOAT
+  libc_crt_target_cflags :=
+  ifeq ($(ARCH_X86_HAVE_SSE2),true)
+      libc_crt_target_cflags += -DUSE_SSE2=1
+  endif
+  ifeq ($(ARCH_X86_HAVE_SSSE3),true)
+      libc_crt_target_cflags += -DUSE_SSSE3=1
+  endif
+endif # x86
+
+ifeq ($(TARGET_ARCH),mips)
+  ifneq ($(ARCH_MIPS_HAS_FPU),true)
+    libc_common_cflags += -DSOFTFLOAT
+  endif
+  libc_common_cflags += -fstrict-aliasing
+  libc_crt_target_cflags := $(TARGET_GLOBAL_CFLAGS)
+endif # mips
 
 # Define ANDROID_SMP appropriately.
 ifeq ($(TARGET_CPU_SMP),true)
