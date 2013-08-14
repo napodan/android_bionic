45bb2bcb9b84bef8f64dde3eeefef8530e4499f3
napodan <david.guenet@gmail.com>
2013-08-14 22:15:14 +0200

    Update some file to 4.3_r2.1

--- libc/include/sys/signalfd.h
+++ libc/include/sys/signalfd.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012 The Android Open Source Project
+ * Copyright (C) 2013 The Android Open Source Project
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -26,34 +26,16 @@
  * SUCH DAMAGE.
  */
 
-#include <sys/types.h>
-#include <private/libc_logging.h>
-#include <stdio.h>
+#ifndef _SYS_SIGNALFD_H_
+#define _SYS_SIGNALFD_H_
 
-/*
- * This source file should only be included by libc.so, its purpose is
- * to support legacy ARM binaries by exporting a publicly visible
- * implementation of atexit().
- */
+#include <signal.h>
+#include <linux/signalfd.h>
 
-extern int __cxa_atexit(void (*func)(void *), void *arg, void *dso);
+__BEGIN_DECLS
 
-/*
- * Register a function to be performed at exit.
- */
-int
-atexit(void (*func)(void))
-{
-    /*
-     * Exit functions queued by this version of atexit will not be called
-     * on dlclose(), and when they are called (at program exit), the
-     * calling library may have been dlclose()'d, causing the program to
-     * crash.
-     */
-    static char const warning[] = "WARNING: generic atexit() called from legacy shared library\n";
+extern int signalfd(int fd, const sigset_t* mask, int flags) __attribute__((__nonnull__(2)));
 
-    __libc_format_log(ANDROID_LOG_WARN, "libc", warning);
-    fprintf(stderr, warning);
+__END_DECLS
 
-    return (__cxa_atexit((void (*)(void *))func, NULL, NULL));
-}
+#endif /* _SYS_SIGNALFD_H */
