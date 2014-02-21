d1c008e4520848c9f7f29d9b9505e3ce8acc903a
napodan <david.guenet@gmail.com>
2014-02-21 12:31:40 +0100

    Update linker/arch/x86 to 4.4.2_r2

--- linker/arch/x86/begin.c
+++ linker/arch/x86/begin.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2008 The Android Open Source Project
+ * Copyright (C) 2012 The Android Open Source Project
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -26,22 +26,31 @@
  * SUCH DAMAGE.
  */
 
-.text
-.align 4
-.type _start, @function
-.globl _start
+#include <stdint.h>
+#include <sys/cdefs.h>
 
-_start:
-        /* save the elfdata ptr to %eax, AND push it onto the stack */
-        mov    %esp, %eax
-        pushl  %esp
+extern unsigned __linker_init(void* raw_args);
 
-        pushl  %eax
-        call   __linker_init
+__LIBC_HIDDEN__ void _start() {
+  void (*start)(void);
 
-        /* linker init returns (%eax) the _entry address in the main image */
-        /* entry point expects sp to point to elfdata */
-        popl   %esp
-        jmp    *%eax
+  void* raw_args = (void*) ((uintptr_t) __builtin_frame_address(0) + sizeof(void*));
+  start = (void(*)(void))__linker_init(raw_args);
 
-#include "__stack_chk_fail_local.S"
+  /* linker init returns (%eax) the _entry address in the main image */
+  /* entry point expects sp to point to raw_args */
+
+  __asm__ (
+     "mov %0, %%esp\n\t"
+     "jmp *%1\n\t"
+     : : "r"(raw_args), "r"(start) :
+  );
+
+  /* Unreachable */
+}
+
+/* Since linker has its own version of crtbegin (this file) it should have */
+/* own version of __stack_chk_fail_local for the case when it's built with */
+/* stack protector feature */
+
+#include "arch-x86/bionic/__stack_chk_fail_local.h"
